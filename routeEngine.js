177ce625b03fbe52e8d884602f534c7e8e2a3ee6
ManvirSDh <manvirdhami4756+school@gmail.com>
2023-10-29 23:07:11 -0700

    added stops to each route

--- routeEngine.js
+++ routeEngine.js
@@ -1,34 +1,36 @@
 const fs = require('fs');
-const routesPath = 'translink_data/routes.txt';
-const calendarDatesPath = 'translink_data/calendar_dates.txt';
-const calendarPath = 'translink_data/calendar.txt';
-const directionsPath = 'translink_data/directions.txt';
-const patternIdPath = 'translink_data/pattern_id.txt';
-const shapesPath = 'translink_data/shapes.txt';
-const stopOrderExceptionsPath = 'translink_data/stop_order_exceptions.txt';
-const stopsPath = 'translink_data/stops.txt';
-const stopTimesPath = 'translink_data/stop_times.txt';
-const transfersPath = 'translink_data/transfers.txt';
-const tripsPath = 'translink_data/trips.txt';
+const routes_path = 'translink_data/routes.txt';
+const calendar_dates_path = 'translink_data/calendar_dates.txt';
+const calendar_path = 'translink_data/calendar.txt';
+const directions_path = 'translink_data/directions.txt';
+const pattern_id_path = 'translink_data/pattern_id.txt';
+const shapes_path = 'translink_data/shapes.txt';
+const stop_order_exceptions_path = 'translink_data/stop_order_exceptions.txt';
+const stops_path = 'translink_data/stops.txt';
+const stop_times_path = 'translink_data/stop_times.txt';
+const transfers_path = 'translink_data/transfers.txt';
+const trips_path = 'translink_data/trips.txt';
+const LOG = true;  
 
 
 
 async function main() {
-    var routes = await parseFile(routesPath);
-    var calendar = parseFile(calendarPath);
-    var calendarDates = parseFile(calendarDatesPath);
-    var directions = await parseFile(directionsPath);
-    var patternId = parseFile(patternIdPath);
-    var shapes = await parseFile(shapesPath);
+    var routes = await parseFile(routes_path);
+    var calendar = parseFile(calendar_path);
+    var calender_dates = parseFile(calendar_dates_path);
+    var directions = await parseFile(directions_path);
+    var pattern_id = parseFile(pattern_id_path);
+    var shapes = await parseFile(shapes_path);
     shapes = combineShapes(shapes);
-    var stopOrderExceptions = parseFile(stopOrderExceptionsPath);
-    var stops = parseFile(stopsPath);
-    var stopTimes = parseFile(stopTimesPath);
-    var transfers = parseFile(transfersPath);
-    var trips = await parseFile(tripsPath);
+    var stop_order_exceptions = parseFile(stop_order_exceptions_path);
+    var stops = parseFile(stops_path);
+    var stop_times = await parseFile(stop_times_path);
+    var transfers = parseFile(transfers_path);
+    var trips = await parseFile(trips_path);
     addShapesToTrips(shapes, trips);
-    addDirectionsToRoutes(directions, trips);
+    addDirectionsToTrips(directions, trips);
     addTripsToRoutes(trips, routes);
+    addStopTimesToTrips(stop_times, trips);
     // console.log(routes);
 }
 
@@ -39,6 +41,10 @@ function parseFile(path)  {
         fs.readFile(path, 'utf8', (err, data) => {
             if (err) {
                 reject(err);
+            }
+
+            if (LOG) {
+                console.log("Parsing File: ", path);
             }
 
             var output = [];
@@ -57,7 +63,7 @@ function parseFile(path)  {
                     temp[titles[j]] = words[j];
                 }
                 output.push(temp);
-            }'﻿direction'
+            }
         
             resolve(output);
         });
@@ -66,6 +72,11 @@ function parseFile(path)  {
 }
 
 function combineShapes(shapes) {
+    if (LOG) {
+        console.log("Combining Shapes");
+    }
+
+    
     var outputShapes = new Object();
     var temp;
     shapes.forEach(point => {
@@ -81,34 +92,67 @@ function combineShapes(shapes) {
 }
 
 function addShapesToTrips(shapes, trips) {
+    if (LOG) {
+        console.log("Adding Shapes to Trips");
+    }
+
     trips.forEach(trip => {
         trip.shape = shapes[trip.shape_id];
     });
 }
 
-function addDirectionsToRoutes(directions, trips) {
+function addDirectionsToTrips(directions, trips) {
+    if (LOG) {
+        console.log("Adding Directions to Trips");
+    }
+
     trips.forEach(trip => {
         for (var i = 0; i < directions.length; i++) {
             if (directions[i].route_id == trip.route_id && directions[i].direction_id == trip.direction_id) {
                 trip.direction = directions[i].direction;
-                if (trip.route_id == 6705) {
-                    console.log(trip);
-                }
+                trip.short_id = directions[i].route_short_name;
+                break;
             }
         }
     });
 }
 
 function addTripsToRoutes(trips, routes) {
+    if (LOG) {
+        console.log("Adding Trips to Routes");
+    }
+
     routes.forEach(route => {
         route.trips = [];
         for (var i = trips.length - 1; i >= 0; i--) {
             if (trips[i].route_id == route.route_id) {
                 route.trips.push(trips[i]);
-                trips.splice(i, 1);
+                if (route.short_id === undefined) {
+                    route.short_id = trips[i].short_id;
+                }
             }
         }
     });
+}
+
+// Holy **** this approach is too slow, maybe do stop centric instead of trip centric organization?
+function addStopTimesToTrips(stop_times, trips) {
+    if (LOG) {
+        console.log("Adding Stop Times to Trips");
+    }
+    for (var j = 0; j < trips.length; j++) {
+        trips[j].stop_times = [];
+    }
+    for (var i = 0; i < stop_times.length; i++) {
+        for (var j = 0; j < trips.length; j++) {
+            if (stop_times[i].trip_id == trips[j].trip_id) {
+                trips[j].stop_times.push(stop_times[i]);
+                stop_times[i].route = trips.route_id;
+                break;
+            }
+        }
+    }
+    console.log(trips[0]);
 }
 
 main();
