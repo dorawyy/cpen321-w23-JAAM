84023f1756a6b992b104bd3d57f254c225d5a5f6
Jayden Wong <jaywong8288@gmail.com>
2023-11-27 18:00:41 -0800

    chore: removes unused imports

--- frontend/app/src/androidTest/java/com/jaam/transittrack/RouteInstrumentedTest.java
+++ frontend/app/src/androidTest/java/com/jaam/transittrack/RouteInstrumentedTest.java
@@ -21,7 +21,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
