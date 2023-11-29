c77b0e9f0d984ab2e34e7fd8bdd37d4d0b8ce830
ManvirSDh <singh@acer.ipv6.telus.net>
2023-11-28 23:33:54 -0800

    fixing syntax in mockChatDB

--- backend/mockChatDB.js
+++ backend/mockChatDB.js
@@ -5,32 +5,32 @@ const mockChatHistory = [
     {
       "_id": "1",
       "text": "Hello, this is from biden at 7:24.",
-      "senderEmail": senderEmail,
-      "receiverEmail": receiverEmail,
+      senderEmail,
+      receiverEmail,
       "timestamp": "2023-11-06T03:22:57.632Z",
       "__v": 0
     },
     {
       "_id": "2",
       "text": "Hello, this is from biden at 7:24.",
-      "senderEmail": senderEmail,
-      "receiverEmail": receiverEmail,
+      senderEmail,
+      receiverEmail,
       "timestamp": "2023-11-06T03:22:57.632Z",
       "__v": 0
     },
     {
       "_id": "3",
       "text": "Hello, this is from biden at 7:24.",
-      "senderEmail": senderEmail,
-      "receiverEmail": receiverEmail,
+      senderEmail,
+      receiverEmail,
       "timestamp": "2023-11-06T03:22:57.632Z",
       "__v": 0
     },
     {
       "_id": "4",
       "text": "Hello, this is from biden at 7:24.",
-      "senderEmail": senderEmail,
-      "receiverEmail": receiverEmail,
+      senderEmail,
+      receiverEmail,
       "timestamp": "2023-11-06T03:22:57.632Z",
       "__v": 0
     },
