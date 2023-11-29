8a97b99bf25c920e43924ceaa424de03227d7095
Jayden Wong <jaywong8288@gmail.com>
2023-11-29 03:19:15 -0800

    feature: cleans up navigation screen

--- frontend/app/src/main/java/com/jaam/transittrack/NavigationActivity.java
+++ frontend/app/src/main/java/com/jaam/transittrack/NavigationActivity.java
@@ -1,20 +1,11 @@
 package com.jaam.transittrack;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-
-import java.io.IOException;
-
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NavigationActivity extends AppCompatActivity {
 
@@ -24,7 +15,7 @@ public class NavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
-        Button chatButton = findViewById(R.id.chatButton);
+
         Button friendListButton = findViewById(R.id.friendListButton);
         friendListButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,35 +35,37 @@ public class NavigationActivity extends AppCompatActivity {
         });
 
         //ChatGPT usage: No
-        chatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent chatIntent = new Intent(NavigationActivity.this, ChatActivity.class);
-                chatIntent.putExtra("receiverEmail", "johndoe@example.com");
-                startActivity(chatIntent);
-
-            }
-        });
+        //DEBUG CODE
+        // Button chatButton = findViewById(R.id.chatButton);
+//        chatButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent chatIntent = new Intent(NavigationActivity.this, ChatActivity.class);
+//                chatIntent.putExtra("receiverEmail", "johndoe@example.com");
+//                startActivity(chatIntent);
+//
+//            }
+//        });
         //ChatGPT usage: No
-        Button notifyButton = findViewById(R.id.notifyButton);
-        notifyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HttpUrl url = HttpUrl.parse("https://20.200.125.197:8081/getFCM").newBuilder()
-                        .addQueryParameter("userEmail", GoogleSignIn.getLastSignedInAccount(NavigationActivity.this).getEmail())
-                        .build();
-                Request request = new Request.Builder()
-                        .url(url)
-                        .get()
-                        .build();
-                Log.d(TAG, request.toString());
-                try {
-                    new OkHttpClient().newCall(request).execute();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+//        Button notifyButton = findViewById(R.id.notifyButton);
+//        notifyButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                HttpUrl url = HttpUrl.parse("https://20.200.125.197:8081/getFCM").newBuilder()
+//                        .addQueryParameter("userEmail", GoogleSignIn.getLastSignedInAccount(NavigationActivity.this).getEmail())
+//                        .build();
+//                Request request = new Request.Builder()
+//                        .url(url)
+//                        .get()
+//                        .build();
+//                Log.d(TAG, request.toString());
+//                try {
+//                    new OkHttpClient().newCall(request).execute();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
 //ChatGPT usage: No
         findViewById(R.id.routeActivityButton).setOnClickListener(new View.OnClickListener() {
             @Override
